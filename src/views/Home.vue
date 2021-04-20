1c1b8ceb8a59151ed7b58a2ceaacf8c6ba0909f8
Aashiq <adams.aashiq@gmail.com>
2021-04-20 15:36:31 +0200

    final commit but blog incomplete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="home">
+  <h1>Home</h1>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="posts.length">
+  <PostList :posts="posts" />
   </div>
+  <div v-else>Loading... </div>
+  <!-- <button @click="showPosts = !showPosts">Toggle posts</button>
+  <button @click="posts.pop()">Delete a post</button> -->
+</div>
+  <!-- <input type="text" v-model="search">
+  <p>Search term - {{ search }}</p>
+  <div v-for="name in matchingNames" :key="name">
+    {{ name }}
+  </div>
+  <button @click="handleClick">Stop watching</button> -->
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import: ref, computed, watch, watchEffect
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { PostList },
+  setup() {
+    const { posts, error, load } = getPosts()
+
+    load()
+
+    //const showPosts = ref(true)
+    return { posts, error }//, showPosts }
   }
+  //   const search = ref('')
+  //   const names = ref(['luffy','zoro','nami','usopp','sanji','chopper','robin','franky','brook'])
+
+  //   const stopWatch = watch(search, () => {
+  //     console.log('watch function ran')
+  //   })
+
+  //   const stopEffect = watchEffect(() => {
+  //     console.log('watchEffect function ran', search.value)
+  //   })
+
+  //   const matchingNames = computed(() => {
+  //     return names.value.filter((name) => name.includes(search.value))
+  //   })
+
+  //   const handleClick = () => {
+  //     stopWatch()
+  //     stopEffect()
+  //   }
+
+  //   return { names, search, matchingNames, handleClick }
+  // }
+
 }
 </script>
